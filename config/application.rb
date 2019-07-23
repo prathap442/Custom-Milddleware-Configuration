23b7f183728fe0277a49d3dcc7c99e30cc25bb96
prathap <prathap.mohan@kloctechnologies.co.in>
2019-07-23 11:12:11 +0530

    added: "changes like configuring the new middleware"
    - Configuring the middleware is the task that needs to be done with the help of application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,10 @@ module Custommiddleware
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    Dir['./app/middlewares/*.rb'].each do |file|
+      require file
+    end
+    config.middleware.use MyMiddleware
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
